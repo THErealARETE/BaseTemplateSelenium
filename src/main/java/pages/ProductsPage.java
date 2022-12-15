3acee3235c812f86421876d144bb3c52dbc6a53e
Amuzu Oluwapelumi <ina_aoluwape@contractor.indeed.com>
2022-12-15 12:40:45 +0100

    Duplo Assestment Soultion Duplo Assestment Soultion Duplo Assestment Solution

--- src/main/java/pages/ProductsPage.java
+++ src/main/java/pages/ProductsPage.java
@@ -15,45 +15,40 @@ public class ProductsPage extends Controller {
     private static final By BACK_TO_PRODUCTS_BUTTON = By.id("back-to-products");
     private static final By FILTER_PRODUCT = By.className("product_sort_container");
     private static final By PRICE_LIST_ARRAY = By.className("inventory_item_price");
-    private static final String SORT_FROM_HIGH_TO_LOW = "hilo";
-
-    private static final String SORT_FROM_LOW_TO_HIGH = "lohi";
+    private static final By DECENDING_ORDER_LIST_ARRAY = By.className("inventory_item_name");
+    private static final String SORT_PRICE_FROM_HIGH_TO_LOW = "hilo";
+    private static final String SORT_ITEMS_IN_ALPHABETICAL_ORDER_DESENDING = "za";
 
     String HIGH_PRICE = "";
     String LOW_PRICE = "";
+    String LOW_ITEM = "";
+    String HIGH_ITEM = "";
 
     public boolean productsTitleIsVisible() {
         return isVisible(PRODUCTS_TITLE);
     }
 
-    public String filterTheProductsFromHighToLow(){
-
-        this.HIGH_PRICE = HIGH_PRICE;
-        this.LOW_PRICE = LOW_PRICE;
+    public void filterTheProductsFromHighToLow(){
 
         Select se = new Select(driver.findElement(FILTER_PRODUCT));
-        se.selectByValue(SORT_FROM_HIGH_TO_LOW);
+        se.selectByValue(SORT_PRICE_FROM_HIGH_TO_LOW);
 
         List<WebElement> allPrices =  driver.findElements(PRICE_LIST_ARRAY);
-        for (WebElement singlePrices : allPrices){
 
-            System.out.println("i got here ");
-            HIGH_PRICE = allPrices.get(0).getText();
-            LOW_PRICE = allPrices.get(allPrices.size() - 1).getText();
-            System.out.println("i got here 2 ");
-            System.out.println(singlePrices.getText());
-        }
-
-        return filterTheProductsFromHighToLow();
+        HIGH_PRICE = allPrices.get(0).getText();
+        LOW_PRICE = allPrices.get(allPrices.size() - 1).getText();
     }
 
-    public String getHIGH_PRICE(){
-        return HIGH_PRICE;
-    }
-    public String getLOW_PRICE(){
-        return LOW_PRICE;
-    }
+    public void filterTheProductsAlphabeticallyDesendingOrder(){
 
+        Select se = new Select(driver.findElement(FILTER_PRODUCT));
+        se.selectByValue(SORT_ITEMS_IN_ALPHABETICAL_ORDER_DESENDING);
+
+        List<WebElement> allItems =  driver.findElements(DECENDING_ORDER_LIST_ARRAY);
+
+        LOW_ITEM = allItems.get(0).getText();
+        HIGH_ITEM = allItems.get(allItems.size() - 1).getText();
+    }
 
     public void clickOnAddToCartBtn() {
         driver.findElement(ADD_TO_CART_BUTTON).click();
@@ -67,4 +62,21 @@ public class ProductsPage extends Controller {
     public boolean backToProductsButtonIsVisible() {
         return isVisible(BACK_TO_PRODUCTS_BUTTON);
     }
+
+
+    public String getHIGH_PRICE(){
+        return HIGH_PRICE;
+    }
+    public String getLOW_PRICE(){
+        return LOW_PRICE;
+    }
+
+    public String getHIGH_ITEM(){
+        return HIGH_ITEM;
+    }
+    public String getLOW_ITEM(){
+        return LOW_ITEM;
+    }
+
+
 }
--- src/test/java/filter/sortItemsInDesendingOrder.java
+++ src/test/java/filter/sortItemsInDesendingOrder.java
@@ -1,3 +1,11 @@
+//package filter;
+//
+//public class sortFromLowToHighTest {
+//
+//
+//}
+
+
 package filter;
 
 import base.BaseTests;
@@ -11,14 +19,15 @@ import java.io.IOException;
 import static util.ConfigReader.getPassword;
 import static util.ConfigReader.getUserName;
 
-public class sortFromHighToLowTest extends BaseTests {
+public class sortItemsInDesendingOrder extends BaseTests {
+
     LoginPage loginPage = new LoginPage();
     ProductsPage productsPage = new ProductsPage();
     @Test
     public void testProductsLoad() throws IOException {
         loginPage.successLogin(getUserName(), getPassword());
-        productsPage.filterTheProductsFromHighToLow();
-        Assert.assertEquals("$49.99" , productsPage.getHIGH_PRICE());
-        Assert.assertEquals("$7.99" , productsPage.getLOW_PRICE());
+        productsPage.filterTheProductsAlphabeticallyDesendingOrder();
+        Assert.assertEquals("Test.allTheThings() T-Shirt (Red)" , productsPage.getLOW_ITEM());
+        Assert.assertEquals("Sauce Labs Backpack" , productsPage.getHIGH_ITEM());
     }
 }
